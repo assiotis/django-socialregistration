84a6c60b06eafdfb41dbfbe64bda30aa6524d391
Marios <assiotis@alum.mit.edu>
2011-10-30 17:03:37 -0700

    Commented out backwards compatibility stuff

--- socialregistration/auth.py
+++ socialregistration/auth.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User
 from socialregistration.contrib.facebook.auth import FacebookAuth
-from socialregistration.contrib.linkedin.auth import LinkedInAuth
+"""from socialregistration.contrib.linkedin.auth import LinkedInAuth
 from socialregistration.contrib.openid.auth import OpenIDAuth
-from socialregistration.contrib.twitter.auth import TwitterAuth
+from socialregistration.contrib.twitter.auth import TwitterAuth"""
 
 import warnings
 
